1c6521a4336e2ca03de55cf2c11b153532649724
elikese <elikese@gmail.com>
2024-03-03 01:16:58 +0900

    lombok 셋팅

--- src/main/java/com/study/blog/test/HttpControllerTest.java
+++ src/main/java/com/study/blog/test/HttpControllerTest.java
@@ -8,21 +8,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest:";
+
     //http://localhost:8080/http/get (select)
     @GetMapping(value = "/http/get")
-    public String getTest() {
-        return "get 요청";
+    public String getTest(Member member) {
+        return "get 요청 : " + member.getId() + " " + member.getUsername() + " " + member.getPassword() + " " + member.getEmail();
     }
+
+
     //http://localhost:8080/http/post (insert)
-    @PostMapping(value = "/http/post")
-    public String postTest() {
-        return "post 요청";
+    @PostMapping(value = "/http/post") // MessageConverter가 자동으로 요청데이터를 Mapping하여 객체로 만들어 줌
+    public String postTest(@RequestBody Member member) {
+        return "post 요청 : " + member.toString();
     }
+
+
     //http://localhost:8080/http/put (update)
     @PutMapping(value = "/http/put")
-    public String putTest() {
-        return "put 요청";
+    public String putTest(@RequestBody Member member) {
+        return "put 요청" + member.toString();
     }
+
+
     //http://localhost:8080/http/delete (delete)
     @DeleteMapping(value = "/http/delete")
     public String deleteTest() {
---
+++ src/main/java/com/study/blog/test/Member.java
@@ -0,0 +1,26 @@
+package com.study.blog.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
